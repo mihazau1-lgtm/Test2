69a886bc5e716e5217d0a6a9c00e51eb1aa5a6e3
mihazau1-lgtm <mihazau1@gmail.com>
2026-02-05 20:49:18 +0300

    Add click event for glitch text and log message

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.glitch-text').addEventListener('click', function() {
+    alert('Welcome to KAKTUS project!');
+    this.style.color = '#fff';
+    this.style.textShadow = '0 0 10px #58a6ff';
+});
+
+console.log("Bio project loaded successfully.");
+
